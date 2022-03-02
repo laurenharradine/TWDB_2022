597f2ffb65578450ff8c950b6de84cd4dee4b96b
Lauren <lauren.ffen@gmail.com>
2022-03-02 13:52:34 +1100

    Made h1 text return to original message on null input

--- section25_DOM_events/input_event/script.js
+++ section25_DOM_events/input_event/script.js
@@ -3,4 +3,8 @@ const h1 = document.querySelector('h1');
 
 input.addEventListener('input', function (e) {
     h1.innerText = `Welcome, ${input.value}`;
+
+    if (input.value === "") {
+      h1.innerHTML = "Enter Your Username";
+    }
 })
