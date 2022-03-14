ec7617a6b74ac922b056b1edcaa1f265b1c6513d
Lauren Harradine <lhharradine@gmail.com>
2022-03-15 10:58:50 +1100

    Added winning score, event listener for P2.

--- section26_scorekeeper/script.js
+++ section26_scorekeeper/script.js
@@ -5,8 +5,15 @@ const p2Display = document.querySelector("#p2Display");
 
 let p1Score = 0;
 let p2Score = 0;
+// not const so that winning score can be changed
+let winningScore = 5;
 
 p1Button.addEventListener('click', function() {
     p1Score += 1;
     p1Display.textContent = p1Score;
+});
+
+p2Button.addEventListener('click', function() {
+    p2Score += 1;
+    p2Display.textContent = p2Score;
 });
