c22a26524036a7e5b5e60a3dbb2d6205ca5d497e
Lauren Harradine <lhharradine@gmail.com>
2022-03-15 10:52:41 +1100

    Added event listener for P1 button.

--- section26_scorekeeper/script.js
+++ section26_scorekeeper/script.js
@@ -1,1 +1,6 @@
-alert("OH HAI!!1!");
+const p1Button = document.querySelector("#p1Button");
+const p2Button = document.querySelector("#p2Button");
+
+p1Button.addEventListener('click', function() {
+    alert("CLICKED");
+});
