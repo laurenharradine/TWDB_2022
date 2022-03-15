d5a94632da08a26eea365ca9cbd93bb76f958a80
Lauren Harradine <lhharradine@gmail.com>
2022-03-15 11:32:04 +1100

    Added event listener for change of winning score.

--- section26_scorekeeper/script.js
+++ section26_scorekeeper/script.js
@@ -3,6 +3,7 @@ const p2Button = document.querySelector("#p2Button");
 const resetButton = document.querySelector("#reset");
 const p1Display = document.querySelector("#p1Display");
 const p2Display = document.querySelector("#p2Display");
+const winningScoreSelect = document.querySelector("#playTo");
 
 let p1Score = 0;
 let p2Score = 0;
@@ -28,6 +29,10 @@ p2Button.addEventListener('click', function() {
         }
         p2Display.textContent = p2Score;
     }
+});
+
+winningScoreSelect.addEventListener('change', function () {
+    alert("CHANGE");
 });
 
 resetButton.addEventListener('click', function () {
