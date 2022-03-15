d083ad0eca26fdbce0f8f0796cb9ba2806e1082f
Lauren Harradine <lhharradine@gmail.com>
2022-03-15 11:11:24 +1100

    Added game over condition.

--- section26_scorekeeper/script.js
+++ section26_scorekeeper/script.js
@@ -7,17 +7,24 @@ let p1Score = 0;
 let p2Score = 0;
 // not const so that winning score can be changed
 let winningScore = 5;
+let isGameOver = false;
 
 p1Button.addEventListener('click', function() {
-    if(p1Score !== winningScore) {
+    if(!isGameOver){
         p1Score += 1;
+        if(p1Score === winningScore) {
+            isGameOver = true;
+        }
         p1Display.textContent = p1Score;
     }
 });
 
 p2Button.addEventListener('click', function() {
-    if(p2Score !== winningScore) {
+    if(!isGameOver){
         p2Score += 1;
+        if(p2Score === winningScore) {
+            isGameOver = true;
+        }
         p2Display.textContent = p2Score;
     }
 });
